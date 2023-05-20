90cc4e9ccbc47d00a9b1b0e255d9402c2f09a369
Aron Peyroteo <aron@mqx.com.br>
2023-05-20 18:28:25 -0300

    SPK-4: phpstan

--- app/Http/Requests/StorePokerTableRequest.php
+++ app/Http/Requests/StorePokerTableRequest.php
@@ -11,6 +11,9 @@ class StorePokerTableRequest extends FormRequest
         return true;
     }
 
+    /**
+     * @return  array<string, array<int, string>>
+     */
     final public function rules(): array
     {
         return [
--- app/Traits/EnumList.php
+++ app/Traits/EnumList.php
@@ -2,16 +2,19 @@
 
 namespace App\Traits;
 
-/**
- * @method static cases()
- */
 trait EnumList
 {
+    /**
+     * @return array<string>
+     */
     public static function listNames(): array
     {
         return array_map(static fn ($enum) => $enum->name, self::cases());
     }
 
+    /**
+     * @return array<string>
+     */
     public static function listValues(): array
     {
         return array_map(static fn ($enum) => $enum->value ?? $enum->name, self::cases());
