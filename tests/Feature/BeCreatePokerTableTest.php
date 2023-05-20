bef6a41d0c93db9d8d9978bf848cf6164b3a9734
Aron Peyroteo <aron@mqx.com.br>
2023-05-20 18:14:16 -0300

    SPK-4: create new poker table UI

--- tests/Feature/BeCreatePokerTableTest.php
+++ tests/Feature/BeCreatePokerTableTest.php
@@ -1,19 +1,24 @@
 <?php
 
-use App\Models\PokerTable;
+use App\Models\{PokerTable, User};
+
+use function Pest\Laravel\actingAs;
 
 test('be create poker table', function () {
-    //ARRANGE
     $pokerTable = PokerTable::factory()->count(1)->make();
+    $user       = User::factory()->create();
 
-    //ACT
+    actingAs($user);
     $response = $this->post(
         route('poker-table.store'),
         $pokerTable->first()->toArray()
     );
 
-    //ASSERT
-    $response->assertStatus(200);
+    $response->assertRedirect(route('poker-table.create'));
     $this->assertDatabaseCount('poker_tables', 1);
+
+    $this->get(
+        route('poker-table.create')
+    )->assertSeeText($pokerTable->first()->name);
 
 });
