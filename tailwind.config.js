6db8e52356a6eb61ba36b085ba5dc49f472d46d0
Aron Peyroteo <aron@mqx.com.br>
2023-05-20 15:24:53 -0300

    SPK-5: Installed daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,32 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
+	content: [
+		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php',
+	],
+	
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	
+	plugins: [
+		forms,
+		require("daisyui")
+	],
+	daisyui: {
+		styled: true,
+		themes: true,
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: "",
+		darkTheme: "dark",
+	},
 };
