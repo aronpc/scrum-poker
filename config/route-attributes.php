f933d3f0a85cfc4f6e0dff49e458a8a295c0154c
Aron Peyroteo <aron@mqx.com.br>
2023-05-20 17:10:39 -0300

    SPK-4: INstalling logviewer and configure route-attributes

--- config/route-attributes.php
+++ config/route-attributes.php
@@ -13,13 +13,13 @@ return [
      * Optionally, you can specify group configuration by using key/values
      */
     'directories' => [
-        app_path('Http/Controllers'),
-        /*
-        app_path('Http/Controllers/Api') => [
-           'prefix' => 'api',
-           'middleware' => 'api',
+        app_path('Http/Controllers') => [
+            'middleware' => 'web',
         ],
-        */
+        app_path('Http/Controllers/Api') => [
+            'prefix'     => 'api',
+            'middleware' => 'api',
+        ],
     ],
 
     /**
